69b698e240e819929c31c4e8007fb4bd9c44f241
Elfira Nurul Ilma <91236883+ilmaelfiraa@users.noreply.github.com>
2022-10-12 17:45:10 +0700

    Add files via upload

---
+++ application/views/pendaftaran/berkas_persyaratan/tambah_berkaspersyaratan.php
@@ -0,0 +1,41 @@
+<div class="header__page d-flex align-items-left align-items-md-center flex-column flex-md-row pt-5 mb-4">
+    <div>
+        <h2 class="fw-bold">Berkas Persyaratan</h2>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb sm-text-rg">
+                <li class="breadcrumb-item"><a href="index.html" class="text-success-700">Dashboard</a></li>
+                <li class="breadcrumb-item">Pendaftaran</li>
+                <li class="breadcrumb-item">Berkas Persyaratan</li>
+                <li class="breadcrumb-item">Tambah</li>
+            </ol>
+        </nav>
+    </div>
+    <div class="ms-md-auto py-2 py-md-0">
+        <a href="<?= base_url('berkas_persyaratan') ?>" class="btn text-bg-white btn-md mb-2"><i
+                class="bx bx-left-arrow-alt pe-2"></i>Batal</a>
+    </div>
+</div>
+
+<div class="col-12 col-md-12 col-lg-12 mb-4">
+    <div class="card">
+        <form action="<?php echo base_url() . 'berkas_persyaratan/tambah'; ?>" method="post"
+            enctype="multipart/form-data">
+            <div class="card-header">
+                <h5 class="fw-medium">Tambah Berkas Persyaratan</h5>
+            </div>
+            <div class="card-body">
+                <div class="col-md-12 offset-md-12">
+                    <div class="form-grup mb-3">
+                        <label-sm for="isi_berkaspersyaratan">Isi Berkas Persyaratan</label-sm>
+                        <textarea name="isi_berkaspersyaratan" class="form-control form-control-md text-secondary-500"
+                            rows="3"></textarea>
+                    </div>
+                    <div class="card-footer text-end">
+                        <button class="btn btn-success text-bg-success-700 btn-md" type="submit"
+                            value="Simpan">Simpan</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
