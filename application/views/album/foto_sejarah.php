8658fd4a309bb1fff806c44452e9aa1c96b5cca3
Elfira Nurul Ilma <91236883+ilmaelfiraa@users.noreply.github.com>
2022-10-12 17:32:53 +0700

    Add files via upload

---
+++ application/views/album/foto_sejarah.php
@@ -0,0 +1,71 @@
+<div class="header__page d-flex align-items-left align-items-md-center flex-column flex-md-row pt-5 mb-4">
+    <div>
+        <h2 class="fw-bold">Foto Sejarah</h2>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb sm-text-rg">
+                <li class="breadcrumb-item"><a href="<?php echo base_url('dashboard') ?>"
+                        class="text-success-700">Dashboard</a></li>
+                <li class="breadcrumb-item">Foto Sejarah</li>
+            </ol>
+        </nav>
+    </div>
+    <div class="ms-md-auto py-2 py-md-0">
+        <a href="<?= base_url('foto_sejarah/tambah') ?>" class="btn text-bg-white btn-md mb-2"><i
+                class="bx bx-plus pe-2"></i>Tambah</a>
+    </div>
+</div>
+<?php $this->load->view('messages') ?>
+<div class="row">
+    <?php foreach ($foto_sejarah as $data) { ?>
+    <div class="col-md-4 mb-4">
+        <div class="card">
+            <div class="card-body">
+                <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active p-1">
+                            <img src="<?php echo base_url('upload/foto_sejarah/' . $data->image) ?>"
+                                class="d-block w-100 rounded-5" alt="thumbnail-img">
+                        </div>
+                    </div>
+                </div>
+                <h6 class="fw-reguler text-truncate mt-3"><?php echo $data->keterangan ?></h6>
+            </div>
+            <div class="card-footer text-end">
+                <a href="" data-bs-toggle="modal"
+                    data-bs-target="#exampleModalConfirm<?php echo ($data->id_fotosejarah) ?>"
+                    class="btn btn-danger text-bg-danger-100 border-0 btn-md">Hapus</a>
+            </div>
+        </div>
+    </div>
+    <?php } ?>
+</div>
+
+
+<?php foreach ($foto_sejarah as $data) { ?>
+<!-- Modal confirm-->
+<div class="modal fade" id="exampleModalConfirm<?php echo ($data->id_fotosejarah) ?>" tabindex="-1"
+    aria-labelledby="exampleModalConfirmLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content custom p-2">
+            <div class="modal-body">
+                <div class="d-flex">
+                    <div class="float-start">
+                        <i class="bi bi-exclamation-circle text-danger pe-4"></i>
+                    </div>
+                    <div class="float-end">
+                        <h5 class="fw-bold text-secondary-700">Anda yakin ingin menghapus data ini?</h5>
+                        <span class="md-text-rg text-secondary-500">Hati-hati! data yang dihapus tidak bisa
+                            dikembalikan</span>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-outline-light btn-md text-secondary-500"
+                    data-bs-dismiss="modal">Cancel</button>
+                <a href="<?php echo base_url('foto_sejarah/delete/' . $data->id_fotosejarah) ?>"
+                    class="btn btn-danger btn-md">Delete</a>
+            </div>
+        </div>
+    </div>
+</div>
+<?php } ?>
---
+++ application/views/album/tambah_fotoprestasisantri.php
@@ -0,0 +1,45 @@
+<div class="header__page d-flex align-items-left align-items-md-center flex-column flex-md-row pt-5 mb-4">
+    <div>
+        <h2 class="fw-bold">Foto Prestasi Santri</h2>
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb sm-text-rg">
+                <li class="breadcrumb-item"><a href="index.html" class="text-success-700">Dashboard</a></li>
+                <li class="breadcrumb-item">Foto Prestasi Santri</li>
+                <li class="breadcrumb-item">Tambah</li>
+            </ol>
+        </nav>
+    </div>
+    <div class="ms-md-auto py-2 py-md-0">
+        <a href="<?= base_url('foto_prestasi_santri') ?>" class="btn text-bg-white btn-md mb-2"><i
+                class="bx bx-left-arrow-alt pe-2"></i>Batal</a>
+    </div>
+</div>
+
+<div class="col-12 col-md-12 col-lg-12 mb-4">
+    <div class="card">
+        <form action="<?php echo base_url() . 'foto_prestasi_santri/tambah'; ?>" method="post"
+            enctype="multipart/form-data">
+            <div class="card-header">
+                <h5 class="fw-medium"><?= $title ?></h5>
+            </div>
+            <div class="card-body">
+                <div class="col-md-12 offset-md-12">
+                    <div class="form-group mb-3">
+                        <label-sm for="image">Gambar</label-sm>
+                        <input type="file" name="image"
+                            class="form-control <?php echo form_error('image') ? 'is-invalid' : '' ?> form-control-md"
+                            required>
+                    </div>
+                    <div class="form-group mb-3">
+                        <label-sm for="keterangan">Keterangan</label-sm>
+                        <input type="text" name="keterangan" class="form-control form-control-md" required>
+                    </div>
+                    <div class="card-footer text-end">
+                        <button class="btn btn-success text-bg-success-700 btn-md" type="submit"
+                            value="Simpan">Simpan</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
