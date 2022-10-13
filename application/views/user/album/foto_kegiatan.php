8a23ef5042efacf7cb3cef97b177a62f21e0a1f0
Elfira Nurul Ilma <91236883+ilmaelfiraa@users.noreply.github.com>
2022-10-13 14:43:25 +0700

    Add files via upload

---
+++ application/views/user/album/foto_kegiatan.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title><?= $title; ?></title>
+
+    <!-- css open img -->
+    <link rel="stylesheet" href="https://rawgit.com/LeshikJanz/libraries/master/Bootstrap/baguetteBox.min.css">
+
+</head>
+
+<body>
+
+    <!-- Title Section Start -->
+    <section class="container title">
+        <!-- Breadcrumb Start-->
+        <nav aria-label="Breadcrumb" class="breadcrumb">
+            <ul>
+                <li><a href="index.html">Beranda</a></li>
+                <li><span aria-current="page">Album</span></li>
+                <li><span aria-current="page"><?php echo $title; ?></span></li>
+            </ul>
+        </nav>
+        <!-- Breadcrumb End-->
+        <h3><?php echo $title; ?></h3>
+    </section>
+    <!-- Title Section End -->
+
+    <div class="container">
+        <div class="tz-gallery">
+            <div class="row">
+                <?php foreach ($foto_kegiatan as $data) : ?>
+                <div class="img_galeri col-sm-6 col-md-4">
+                    <a class="lightbox" href="<?php echo base_url('upload/foto_kegiatan/' . $data->image) ?>">
+                        <img src="<?php echo base_url('upload/foto_kegiatan/' . $data->image) ?>" alt="">
+                    </a>
+                    <h4 class="mt-4"><?php echo $data->keterangan ?></h4>
+                </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </div>
+
+    <!-- SCROLL REVEAL -->
+    <script src="https://unpkg.com/scrollreveal"></script>
+    <!-- SHOW IMG -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.js"></script>
+    <script>
+    baguetteBox.run('.tz-gallery');
+    </script>
+</body>
+
+</html>
