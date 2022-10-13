711adaf21113f050bdaae6d5546af4e43ed8f2e9
Elfira Nurul Ilma <91236883+ilmaelfiraa@users.noreply.github.com>
2022-10-13 14:53:58 +0700

    Create fungsi_helper.php

---
+++ application/helpers/fungsi_helper.php
@@ -0,0 +1,34 @@
+<?php
+
+function check_already_login()
+{
+    $ci = &get_instance();
+    $user_session = $ci->session->userdata('iduser');
+    if ($user_session) {
+        redirect('dashboard');
+    }
+}
+
+
+function check_not_login()
+{
+    $ci = &get_instance();
+    $user_session = $ci->session->userdata('iduser');
+    if (!$user_session) {
+        redirect('auth/login');
+    }
+}
+
+function check_admin()
+{
+    $ci = &get_instance();
+    $ci->load->library('fungsi');
+    if ($ci->fungsi->user_login()->level != 1) {
+        redirect('dashboard');
+    }
+}
+
+function changeDateFormat($format = '%B %d, %Y', $originalDate)
+{
+    return date($format, strtotime($originalDate));
+}
